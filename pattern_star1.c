2340127f4e37ed1762c885767ea668afc4e62025
kassturii <nitu81086@gmail.com>
2026-09-14 23:16:43 +0530

    Add star pattern printing in pattern_star1.c
    Implement a simple C program to print a star pattern.

---
+++ pattern_star1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i ,j;
+    for (i=1;i<=5;i++){
+        printf("\n");
+        for (j=1;j<=10;j++)
+            printf("*");
+    }
+    return 0;
+}
