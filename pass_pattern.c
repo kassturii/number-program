9a2ee90a455933529534f3bb427614bdb6ad0121
kassturii <nitu81086@gmail.com>
2026-09-14 23:15:20 +0530

    Add pattern printing in pass_pattern.c
    Implement a pattern printing program in C.

---
+++ pass_pattern.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i ,j;
+    for (i=1;i<=5;i++){
+        printf("\n pass %d -",i);
+        for (j=1;j<=5;j++)
+            printf("%d",j);
+    }
+    return 0;
+}
+    
