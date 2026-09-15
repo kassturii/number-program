909b534cdf51a1eca7925d13212b48b4815e0730
kassturii <nitu81086@gmail.com>
2026-09-15 20:39:33 +0530

    Create alphabet_pattern1.c

---
+++ alphabet_pattern1.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char i ,j;
+   for (i=65;i<70;i++){
+       for (j=65;j<=i;j++)
+           {
+               printf("%c",j);
+           }
+   
+    printf("\n");
+   }
+    return 0;
+}
