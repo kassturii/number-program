4656a78ff2fd7741d18ba98b80e9aa19660f8755
kassturii <nitu81086@gmail.com>
2026-09-17 11:08:52 +0530

    Add child alphabet learning system in C.c
    Implement a basic alphabet learning system for children.

---
+++ child_alphabetlearning system.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char ch;
+    printf("CHILD ALPHABET LEARNING SYSTEM:");
+    scanf("%c",&ch);
+    switch(ch){
+        case 'A':
+        printf("A FOR APPLE!");
+        break;
+         case 'B':
+        printf("B FOR BALL!");
+        break;
+        case 'C':
+        printf("B FOR BALL!");
+        break;
+    default:
+        printf("Please enter A ,B OR C!!");
+        
+    }     
+
+    return 0;
+}
